8cbaa10ed2cf723b27b1e576039da6ebc9cafe3f
mco <muslumcanozata@gmail.com>
2024-06-30 23:13:31 +0300

    getting records by using header names correctly

--- src/main/java/com/api/compesations/constants/HeaderNames.java
+++ src/main/java/com/api/compesations/constants/HeaderNames.java
@@ -8,7 +8,7 @@ public class HeaderNames {
     public static final String ANNUAL_SALARY = "What is your annual salary?";
     public static final String CURRENCY = "Please indicate the currency";
     public static final String LOCATION = "Where are you located? (City/state/country)";
-    public static final String EXPERIENCE = "How many years of experience do you have?";
-    public static final String JOB_TITLE_CONTEXT = "\"If your job title needs additional context, please clarify here:\"";
-    public static final String OTHER_CURRENCY = "\"If \"\"Other,\"\" please indicate the currency here: \"";
+    public static final String EXPERIENCE = "How many years of post-college professional work experience do you have?";
+    public static final String JOB_TITLE_CONTEXT = "If your job title needs additional context, please clarify here:";
+    public static final String OTHER_CURRENCY = "If \"Other,\" please indicate the currency here:";
 }

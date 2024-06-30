cacc217f8d166b372573f525ae3d0ddb10e478e7
mco <muslumcanozata@gmail.com>
2024-06-30 19:22:24 +0300

    warning fix

--- src/main/java/com/api/compesations/messaging/publisher/CompensationCreatedPublisher.java
+++ src/main/java/com/api/compesations/messaging/publisher/CompensationCreatedPublisher.java
@@ -7,8 +7,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor

2c2cbaed0542ec40a6ce7763308d387fc1d679c2
mco <muslumcanozata@gmail.com>
2024-06-30 19:33:40 +0300

    implementing file operation methods

---
+++ src/main/java/com/api/compesations/util/CsvUtil.java
@@ -0,0 +1,57 @@
+package com.api.compesations.util;
+
+import com.api.compesations.constants.HeaderNames;
+import com.api.compesations.domain.entity.Compensation;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvUtil {
+    public static String TYPE = "text/csv";
+
+    public static boolean hasCsvFormat(MultipartFile file) {
+        if (!TYPE.equals(file.getContentType())) {
+            return false;
+        }
+        return true;
+    }
+
+    public static List<Compensation> csvToCompensationList(InputStream is) {
+        try (BufferedReader bReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+             CSVParser csvParser = new CSVParser(bReader,
+                     CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
+            List<Compensation> compensations = new ArrayList<>();
+            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+            for (CSVRecord csvRecord : csvRecords) {
+                Compensation compensation = toCompensation(csvRecord);
+                compensations.add(compensation);
+            }
+            return compensations;
+        } catch (IOException e) {
+            throw new RuntimeException("CSV data is failed to parse: " + e.getMessage());
+        }
+    }
+
+    private static Compensation toCompensation(CSVRecord csvRecord) {
+        Compensation compensation = new Compensation();
+        compensation.setAge(Integer.parseInt(csvRecord.get(HeaderNames.AGE)));
+        compensation.setCurrency(csvRecord.get(HeaderNames.CURRENCY));
+        compensation.setAnnualSalary(new BigDecimal(csvRecord.get(HeaderNames.ANNUAL_SALARY)));
+        compensation.setIndustry(csvRecord.get(HeaderNames.INDUSTRY));
+        compensation.setJobTitle(csvRecord.get(HeaderNames.JOB_TITLE));
+        compensation.setYearsExperience(Integer.parseInt(csvRecord.get(HeaderNames.EXPERIENCE)));
+        compensation.setJobTitleContext(csvRecord.get(HeaderNames.JOB_TITLE_CONTEXT));
+        compensation.setLocation(csvRecord.get(HeaderNames.LOCATION));
+        compensation.setOtherCurrency(csvRecord.get(HeaderNames.OTHER_CURRENCY));
+        compensation.setTimestamp(LocalDateTime.parse(csvRecord.get(HeaderNames.TIMESTAMP)));
+        return compensation;
+    }
+}
+
